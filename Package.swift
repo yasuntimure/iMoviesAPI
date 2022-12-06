16f070686cc8a2a7290ccb5c375ce3ec3e19511c
eyupyasuntimur <eyup.yasuntimur@rempeople.com>
2022-12-06 22:54:23 +0300

    target dependency added

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "iMoviesAPI",
-            dependencies: []),
+            dependencies: ["Alamofire.git"]),
         .testTarget(
             name: "iMoviesAPITests",
             dependencies: ["iMoviesAPI"]),
