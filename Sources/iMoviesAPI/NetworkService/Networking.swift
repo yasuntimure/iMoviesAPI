1faaa6df7115d4e13a27d093f5f69094bda32cc0
Eyup Yasuntimur <yasuntimure@gmail.com>
2023-09-28 16:16:17 -0700

    Networking class relocated

---
+++ Sources/iMoviesAPI/NetworkService/Networking.swift
@@ -0,0 +1,38 @@
+//
+//  File.swift
+//  
+//
+//  Created by Eyüp on 2023-09-28.
+//
+
+import Foundation
+
+// MARK: - NetworkingProtocol
+
+public protocol NetworkingProtocol {
+    func request<T: Decodable>(_ endpoint: Endpoint) async throws -> T
+}
+
+// MARK: - Networking Class
+
+public final class Networking: NetworkingProtocol {
+    public func request<T: Decodable>(_ endpoint: Endpoint) async throws -> T {
+        guard let request = endpoint.request else { throw NetworkError.invalidRequest }
+
+        let (data, response) = try await URLSession.shared.data(for: request)
+
+        guard let httpResponse = response as? HTTPURLResponse else { throw NetworkError.responseUnsuccessful }
+
+        switch httpResponse.statusCode {
+        case 200...299:
+            let responseObject = try JSONDecoder().decode(T.self, from: data)
+            return responseObject
+        case 400...499:
+            throw NetworkError.notFound
+        case 500...599:
+            throw NetworkError.badRequest
+        default:
+            throw NetworkError.unknownError
+        }
+    }
+}
