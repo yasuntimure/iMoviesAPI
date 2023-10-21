2c91104e9e95e1e329c9c70248b1f185802222cc
Eyup Yasuntimur <yasuntimure@gmail.com>
2023-10-21 09:59:20 -0700

    create FeedItem

---
+++ Sources/iMoviesAPI/Models/FeedItem.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Eyüp on 2023-10-21.
+//
+
+import Foundation
---
+++ Sources/iMoviesAPI/Services/RemoteFeedLoader.swift
@@ -0,0 +1,20 @@
+//
+//  File.swift
+//  
+//
+//  Created by Eyüp on 2023-10-17.
+//
+
+import Foundation
+
+ struct FeedItem {
+    let id: UUID
+    let title: String?
+    let releaseDate: String?
+    let overview: String?
+    let popularity: Int?
+}
+
+class RemoteFeedLoader {
+
+}
